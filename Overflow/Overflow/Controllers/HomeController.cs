7d498b5cdd915bd40afd7c138b09e42bb487331d
Wahlrus <Justjason24@gmail.com>
2019-09-11 13:14:14 -0500

    data binding to login object from login modal and is sent to the controller for validation.

--- Overflow/Overflow/Controllers/HomeController.cs
+++ Overflow/Overflow/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Overflow.Models;
 
 namespace Overflow.Controllers
 {
@@ -25,6 +26,18 @@ namespace Overflow.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Login(Login login)
+        {
+            /* 
+             Use login.Email to access email entered, and login.Pass to access password entered.
+             If email does not pass the required criteria, return a error modal.
+             Else, call query to access db.
+             
+             */
+            
+            return Content(login.Email + login.Pass);
         }
     }
 }

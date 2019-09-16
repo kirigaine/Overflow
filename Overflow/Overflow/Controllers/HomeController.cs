23f4a25a7ac8a6925ab52f4d8cef0453d914bab4
Tanner Bishop <tbishop_1996@hotmail.com>
2019-09-16 15:08:36 -0500

    added login error modal; minor changes

--- Overflow/Overflow/Controllers/HomeController.cs
+++ Overflow/Overflow/Controllers/HomeController.cs
@@ -14,20 +14,6 @@ namespace Overflow.Controllers
             return View();
         }
 
-        public ActionResult newRecipes()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult advSearch()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
         public ActionResult Login(Login login)
         {
             /* 

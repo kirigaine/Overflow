9b31dd2c3b5a2c45d5bc8ed0c70aee19bab89afa
Devin Cook <devin.cook@icloud.com>
2019-10-08 22:13:08 -0500

    Inventory table and items update

---
+++ Overflow/SQLQuery1.sql
@@ -0,0 +1,14 @@
+﻿USE [Overflow]
+GO
+
+DECLARE	@return_value Int
+
+EXEC	@return_value = [dbo].[insertNewAccountProc]
+		@uName_proc_param = N'devin.cook@icloud.com',
+		@pw_proc_param = N'1234567890',
+		@fName_proc_param = N'Devin',
+		@lName_proc_param = N'Cook'
+
+SELECT	@return_value as 'Return Value'
+
+GO

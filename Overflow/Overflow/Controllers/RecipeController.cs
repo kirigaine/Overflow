a8a40dba80e9c664554a8ecf92687f240523bf0d
Jordan Powell <jfp88530@ucmo.edu>
2019-11-04 14:41:49 -0600

    Here's the recipe controller

---
+++ Overflow/Overflow/Controllers/RecipeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Overflow.Controllers
+{
+    public class RecipeController : Controller
+    {
+        // GET: Recipe
+        public ActionResult Recipe()
+        {
+            return View();
+        }
+    }
+}
